6e295ecb8d488a1bb05d1a3926ad985ff11116a4
Curtis Schembri <curtisschembri@outlook.com>
2023-11-06 14:37:26 +1000

    file upload

--- src/app.js
+++ src/app.js
@@ -8,11 +8,12 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectionLimit: 10
 });
 
 db.connect((err) => {
@@ -124,4 +125,64 @@ app.post('/login', async (req, res) => {
       }
     });
   });
+});
+
+// Create a multer-s3 storage instance for file uploads
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'cschembri-project', // Set your S3 bucket name
+    acl: 'public-read', // Set appropriate ACL permissions
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      // Authenticate the user and construct the S3 object key
+      authenticateUserAndGenerateKey(req.body.email, req.body.password, file, cb);
+    },
+  }),
+});
+
+// Authenticate the user and generate the S3 object key
+async function authenticateUserAndGenerateKey(email, password, file, cb) {
+  const connection = await pool.getConnection();
+
+  try {
+    // Query the database to retrieve user data
+    const [results] = await connection.execute('SELECT email, password FROM user WHERE email = ?', [email]);
+
+    if (results.length === 0) {
+      // User not found
+      return cb(new Error('User not found'));
+    }
+
+    const user = results[0];
+    const storedPasswordHash = user.password;
+
+    // Compare the provided password with the stored password hash
+    bcrypt.compare(password, storedPasswordHash, (bcryptErr, match) => {
+      if (bcryptErr || !match) {
+        return cb(new Error('Authentication failed'));
+      }
+
+      // Passwords match, authentication successful
+      const userEmail = user.email;
+      const fileExt = file.originalname.split('.').pop();
+      const key = `${userEmail}/${Date.now()}-${Math.floor(Math.random() * 1000)}.${fileExt}`;
+      cb(null, key);
+    });
+  } catch (err) {
+    cb(err);
+  } finally {
+    connection.release();
+  }
+}
+
+// Define the /upload route for authentication and file upload
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+
+  return res.status(200).json({ message: 'File uploaded to S3 bucket successfully' });
 });
