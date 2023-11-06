8f40c12a863872e5a25e295432efd48bebb2e039
Curtis Schembri <curtisschembri@outlook.com>
2023-11-06 13:59:07 +1000

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+This application requires a `.env` file in the root directory to run locally, with the following keys set:
+1. AWS_ACCESS_KEY_ID
+2. AWS_SECRET_ACCESS_KEY
+3. AWS_SESSION_TOKEN
+4. AWS_REGION
+5. OPEN_AI_CHATGPT_API_KEY
---
+++ src/app.js
@@ -0,0 +1,97 @@
+require("dotenv").config();
+
+const express = require('express');
+const axios = require('axios');
+const { createS3Bucket, uploadJsonToS3, getObjectFromS3 } = require('./aws'); 
+const cors = require('cors');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+
+const db = mysql.createConnection({
+  host: 'cschembri-database.ce2haupt2cta.ap-southeast-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Callofduty0641$',
+  database: 'project',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+//
+// AWS config.
+//
+const bucket = "cschembri-docker-mashup";
+const key = "counter.json";
+
+//
+// Setup Express.
+//
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+const port = 3001;
+
+//
+// Main server entry point.
+//
+app.listen(port, async () => {
+  console.log(`Server is running at http://localhost:${port}`);
+
+  //console.log("Verifying AWS functionality...")
+  //const defaultBody = { counter: 0 }
+  //await createS3Bucket(bucket);
+  //await uploadJsonToS3(bucket, key, defaultBody);
+  //await getObjectFromS3(bucket, key).then((object) => console.log(object));
+  //console.log("AWS functionality verified.")
+});
+
+// POST /login route
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  const SELECT_QUERY = 'SELECT id, email, password FROM users WHERE email = ?';
+
+  // Query the database to retrieve the user's information
+  db.query(SELECT_QUERY, [email], (err, results) => {
+    if (err) {
+      console.error('Error querying the database: ', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    if (results.length === 0) {
+      // User not found
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    const user = results[0];
+    const storedPasswordHash = user.password;
+
+    // Compare the provided password with the stored password hash
+    bcrypt.compare(password, storedPasswordHash, (bcryptErr, match) => {
+      if (bcryptErr) {
+        console.error('Error comparing password hashes: ', bcryptErr);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      if (match) {
+        // Passwords match, authentication successful
+        return res.status(200).json({ message: 'Login successful' });
+      } else {
+        // Passwords do not match, authentication failed
+        return res.status(401).json({ message: 'Authentication failed' });
+      }
+    });
+  });
+});
